4ff61201102be29eefbf2f04f58d30bb99507e19
Ketse <snunleys@gmail.com>
2016-10-01 22:28:39 -0400

    beautifier

--- www/js/base.js
+++ www/js/base.js
@@ -36,8 +36,8 @@ google.devrel.samples.hello.SCOPES =
  * Prints a greeting to the greeting log.
  * param {Object} greeting Greeting to print.
  */
-google.devrel.samples.hello.print = function(greeting) {
-    $.each(greeting.result.items, function(i, val) {
+google.devrel.samples.hello.print = function (greeting) {
+    $.each(greeting.result.items, function (i, val) {
         var element = document.createElement('div');
         element.classList.add('row');
         element.innerHTML = 'Game: ' + i + ' and it is ' + val.turn + '\'s turn';
@@ -49,22 +49,22 @@ google.devrel.samples.hello.print = function(greeting) {
  * Gets a numbered greeting via the API.
  * @param {string} id ID of the greeting.
  */
-google.devrel.samples.hello.getGreeting = function() {
+google.devrel.samples.hello.getGreeting = function () {
     console.log("Here is a list of the entire API");
     console.log(gapi.client.word_bait);
     gapi.client.word_bait.get_games().execute(
-    function(resp) {
-        if (resp.result) {
-            google.devrel.samples.hello.print(resp);
-        }
-    });
+        function (resp) {
+            if (resp.result) {
+                google.devrel.samples.hello.print(resp);
+            }
+        });
 };
 
 /**
  * Enables the button callbacks in the UI.
  */
-google.devrel.samples.hello.enableButtons = function() {
-    $("#test").on("click", function(e) {
+google.devrel.samples.hello.enableButtons = function () {
+    $("#test").on("click", function (e) {
         e.preventDefault();
         google.devrel.samples.hello.getGreeting();
     });
@@ -74,19 +74,19 @@ google.devrel.samples.hello.enableButtons = function() {
  * Initializes the application.
  * @param {string} apiRoot Root of the API's path.
  */
-google.devrel.samples.hello.init = function(apiRoot) {
-  // Loads the OAuth and helloworld APIs asynchronously, and triggers login
-  // when they have completed.
-  var apisToLoad;
-  var callback = function() {
-    if (--apisToLoad === 0) {
-      google.devrel.samples.hello.enableButtons();
-    }
-  };
+google.devrel.samples.hello.init = function (apiRoot) {
+    // Loads the OAuth and helloworld APIs asynchronously, and triggers login
+    // when they have completed.
+    var apisToLoad;
+    var callback = function () {
+        if (--apisToLoad === 0) {
+            google.devrel.samples.hello.enableButtons();
+        }
+    };
 
-  apisToLoad = 1; // must match number of calls to gapi.client.load()
-  gapi.client.load('word_bait', 'v1', callback, apiRoot);
-//  gapi.client.load('oauth2', 'v2', callback);
+    apisToLoad = 1; // must match number of calls to gapi.client.load()
+    gapi.client.load('word_bait', 'v1', callback, apiRoot);
+    //  gapi.client.load('oauth2', 'v2', callback);
 };
 
 function init() {
